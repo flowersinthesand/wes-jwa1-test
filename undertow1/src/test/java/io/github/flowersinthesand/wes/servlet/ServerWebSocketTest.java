c7db4e07df85e3078292932a34ba47da79d45340
Donghwan Kim <flowersinthesand@gmail.com>
2014-01-25 10:27:15 +0900

    Add Undertow 1 test

---
+++ undertow1/src/test/java/io/github/flowersinthesand/wes/servlet/ServerWebSocketTest.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2013-2014 Donghwan Kim
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.flowersinthesand.wes.servlet;
+
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.assertThat;
+import io.github.flowersinthesand.wes.Action;
+import io.github.flowersinthesand.wes.ServerWebSocket;
+import io.github.flowersinthesand.wes.jwa.JwaBridge;
+import io.github.flowersinthesand.wes.test.ServerWebSocketTestTemplate;
+import io.undertow.Undertow;
+import io.undertow.servlet.Servlets;
+import io.undertow.servlet.api.DeploymentInfo;
+import io.undertow.servlet.api.DeploymentManager;
+import io.undertow.websockets.jsr.WebSocketDeploymentInfo;
+
+import javax.servlet.ServletException;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpointConfig;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class ServerWebSocketTest extends ServerWebSocketTestTemplate {
+
+	Undertow server;
+
+	@Override
+	protected void startServer() throws ServletException {
+		ServerEndpointConfig config = new JwaBridge("/test").websocketAction(new Action<ServerWebSocket>() {
+			@Override
+			public void on(ServerWebSocket ws) {
+				performer.serverAction().on(ws);
+			}
+		})
+		.config();
+		
+		DeploymentInfo builder = Servlets.deployment()
+			.setClassLoader(ServerWebSocketTest.class.getClassLoader())
+			.setContextPath("/")
+			.addServletContextAttribute(WebSocketDeploymentInfo.ATTRIBUTE_NAME,  new WebSocketDeploymentInfo().addEndpoint(config))
+			.setDeploymentName("test.war");
+		
+		DeploymentManager manager = Servlets.defaultContainer().addDeployment(builder);
+        manager.deploy();
+        
+		server = Undertow.builder()
+			.addHttpListener(port, "localhost")
+			.setHandler(manager.start())
+			.build();
+		server.start();
+	}
+	
+	@Test
+	public void unwrap() {
+		performer.serverAction(new Action<ServerWebSocket>() {
+			@Override
+			public void on(ServerWebSocket ws) {
+				assertThat(ws.unwrap(Session.class), instanceOf(Session.class));
+				performer.start();
+			}
+		})
+		.connect();
+	}
+	
+	// Fixed in 1.0.0.Beta34
+	// https://issues.jboss.org/browse/UNDERTOW-177
+	@Override
+	@Test
+	@Ignore
+	public void uri() {}
+
+	@Override
+	protected void stopServer() {
+		server.stop();
+	}
+
+}
